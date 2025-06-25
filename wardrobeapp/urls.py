bec643229694707c10a58b8ac2b1e9dc7aff5089
martiiann <martiian03817@icloud.com>
2025-06-25 16:30:45 +0100

    Initial project setup with base template, home page, and hero design

--- wardrobeapp/urls.py
+++ wardrobeapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('shop/', views.shop, name='shop'),
+    path('shop/mens/', views.mens_clothing, name='mens_clothing'),
+    path('shop/womens/', views.womens_clothing, name='womens_clothing'),
 ]
--- wardrobeapp/views.py
+++ wardrobeapp/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def shop(request):
+    return render(request, 'shop.html')
+
+def mens_clothing(request):
+    return render(request, 'mens.html')  # You'll create this page later
+
+def womens_clothing(request):
+    return render(request, 'womens.html')  # You'll create this page later
