b5a73f6432deb3343a3065024f25bfdd1f01edc1
Mykyta Martiian <martiiannn@Mykytas-MacBook-Air.local>
2025-08-06 00:29:43 +0100

    test: add failing test for Product model string representation

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from products.models import Category, Product
 
-# Create your tests here.
+class ProductModelTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Men's Clothing")
+
+    def test_product_str(self):
+        product = Product.objects.create(
+            name="Test Jacket",
+            category=self.category,
+            description="Test description",
+            price=49.99,
+            is_available=True,
+            gender="M"
+        )
+        self.assertEqual(str(product), "Test Jacket")
