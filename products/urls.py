96c6a0b32d23d26f150c219b90485d198b8f3ebe
Mykyta Martiian <martiiannn@Mykytas-Air.lan>
2025-08-06 23:49:06 +0100

    Fix product detail page 404 error

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.shop_view, name='shop'),
     path('shop/<str:gender>/<str:category>/', views.product_list_by_gender, name='product_list_by_category'),
     path('shop/<str:gender>/', views.product_list_by_gender, name='product_list_by_gender'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('shop/product/<int:pk>/', views.product_detail, name='product_detail'),
     path('shop/', views.product_list, name='product_list'),  # optional fallback
 ]
